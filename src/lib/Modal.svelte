8aeabb23a48ed0e65b2c935b422f1d13f85ff530
Matias Meno <m@tias.me>
2022-04-19 16:29:11 +0200

    Add help

---
+++ src/lib/Modal.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+  import CloseIcon from '~icons/ion/close-circle-outline'
+
+  const dispatch = createEventDispatcher<{
+    close: void
+  }>()
+</script>
+
+<section>
+  <div class="backdrop" on:click={() => dispatch('close')} />
+  <div class="content">
+    <button class="close" on:click={() => dispatch('close')}
+      ><CloseIcon /></button
+    >
+    <slot />
+  </div>
+</section>
+
+<style lang="postcss">
+  section {
+    position: fixed;
+    inset: 0;
+    z-index: 10000;
+    display: grid;
+    place-content: center;
+    padding: 24px;
+  }
+  .close {
+    all: unset;
+    position: absolute;
+    right: 12px;
+    top: 12px;
+    background: transparent;
+    font-size: 2em;
+    cusor: pointer;
+  }
+  .backdrop {
+    position: absolute;
+    inset: 0;
+    background: #0007;
+  }
+  .content {
+    position: relative;
+    width: 100%;
+    max-width: 600px;
+    background: white;
+    border-radius: 5px;
+    padding: 24px;
+  }
+</style>
--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,13 +1,50 @@
 <script lang="ts">
   import HelpIcon from '~icons/ion/md-help-circle-outline'
   import StatsIcon from '~icons/ion/stats-bars'
+  import Modal from './Modal.svelte'
+
+  let showHelp = false
 </script>
 
 <nav>
-  <HelpIcon />
+  <a href="#" on:click|preventDefault={() => (showHelp = true)}><HelpIcon /></a>
   <strong>Melodle</strong>
   <StatsIcon />
 </nav>
+
+{#if showHelp}
+  <Modal on:close={() => (showHelp = false)}>
+    <h1>Rules</h1>
+
+    <p>Guess the melody in 6 tries.</p>
+
+    <p>
+      Before each guess, you can play the melody <strong>once</strong>. I
+      recommend to listen to the reference C' tone to make sure your audio is
+      working.
+    </p>
+
+    <h2>Mobile users</h2>
+
+    <p>
+      Make sure your phone is not on silent mode, otherwise you won't be able to
+      hear anything.
+    </p>
+
+    <h2>Desktop users</h2>
+    <p>
+      If you're using a keyboard, you can use the corresponding keys. To get a
+      sharp note, you can use the up / down arrow keys.
+    </p>
+    <hr />
+    <p>
+      After each guess, the color of the tiles will change to show how close
+      your guess was to the notes.
+    </p>
+
+    <p>A new Melodle is available each day.</p>
+  </Modal>
+{/if}
 
 <style>
   nav {
