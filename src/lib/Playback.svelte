793a499963abd87c2073059ade2f163c940557c0
Matias Meno <m@tias.me>
2022-04-22 12:53:22 +0200

    Use nice piano sound

--- src/lib/Playback.svelte
+++ src/lib/Playback.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
   import { onMount } from 'svelte'
+  import type { Sampler } from 'tone'
   import * as Tone from 'tone'
   import PlayIcon from '~icons/ion/play'
   import { getNotation } from './core/note'
   import { board } from './stores/board'
 
-  let synth: Tone.Synth | undefined
+  let sampler: Sampler
+
+  // let synth: Tone.Synth | undefined
 
   const tempo = 100
 
@@ -14,35 +17,47 @@
     const lastGuess = $board.guesses[$board.guesses.length - 1]
     melodyPlayed = lastGuess && lastGuess.played && !lastGuess.submitted
   }
-
   onMount(() => {
-    return () => synth?.dispose()
+    sampler = new Tone.Sampler({
+      urls: {
+        A4: 'A4.mp3',
+        C4: 'C4.mp3',
+        'D#4': 'Ds4.mp3',
+        'F#4': 'Fs4.mp3',
+      },
+      baseUrl: 'https://tonejs.github.io/audio/salamander/',
+      onload: () => {
+        console.log('loaded')
+        Tone.Transport.bpm.setValueAtTime(tempo, Tone.now())
+      },
+    }).toDestination()
+    return () => sampler.dispose()
   })
 
   const playReference = () => {
+    if (!sampler.loaded) {
+      window.alert('Audio not loaded yet')
+      return
+    }
     requireReference = false
-    synth?.dispose()
-    synth = new Tone.Synth().toDestination()
     const now = Tone.now()
 
-    Tone.Transport.bpm.setValueAtTime(tempo, now)
-
-    synth.triggerAttackRelease('C4', '4n', now)
+    sampler.triggerAttackRelease('C4', '2n', now)
   }
   const play = () => {
     if (requireReference || melodyPlayed) return
+    if (!sampler.loaded) {
+      window.alert('Audio not loaded yet')
+      return
+    }
     board.melodyPlayed()
-    synth?.dispose()
-    synth = new Tone.Synth().toDestination()
     const now = Tone.now()
     const time = Tone.Time('8n').toSeconds()
 
-    Tone.Transport.bpm.setValueAtTime(tempo, now)
-
     $board.melody.forEach((semitone, i) => {
-      synth.triggerAttackRelease(
+      sampler.triggerAttackRelease(
         getNotation(semitone, { octave: true }),
-        '8n',
+        '4n',
         now + time * i,
       )
     })
