43a9244e191cef2bf441347cdff45d74bbafb283
Matias Meno <m@tias.me>
2022-04-24 17:09:25 +0200

    Remove client https redirect

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,13 +1,5 @@
 <script>
   import '$lib/style/style.css'
-
-  if (
-    typeof window !== 'undefined' &&
-    window.location.protocol !== 'https' &&
-    !window.location.host === 'melodle.yesmeno.com'
-  ) {
-    window.location.replace(window.location.href.replace('http', 'https'))
-  }
 </script>
 
 <svelte:head>
